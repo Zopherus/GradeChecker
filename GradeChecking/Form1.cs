3940e6181176d853761c7fe0ac68273957e84238
unknown <1012746@IHS60261436.ad.nsd.org>
2016-05-10 13:56:30 -0700

    a

--- GradeChecking/Form1.cs
+++ GradeChecking/Form1.cs
@@ -21,31 +21,31 @@ namespace GradeChecking
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            string formUrl = "https://grades.nsd.org/index.php"; // NOTE: This is the URL the form POSTs to, not the URL of the form (you can find this in the "action" attribute of the HTML's form tag
-            string formParams = string.Format("email_address={0}&password={1}", "your email", "your password");
-            string cookieHeader;
-            WebRequest req = WebRequest.Create(formUrl);
-            req.ContentType = "application/x-www-form-urlencoded";
-            req.Method = "POST";
-            byte[] bytes = Encoding.ASCII.GetBytes(formParams);
-            req.ContentLength = bytes.Length;
-            using (Stream os = req.GetRequestStream())
-            {
-                os.Write(bytes, 0, bytes.Length);
-            }
-            WebResponse resp = req.GetResponse();
-            cookieHeader = resp.Headers["Set-cookie"];
+			string formUrl = "https://grades.nsd.org/login.php"; // NOTE: This is the URL the form POSTs to, not the URL of the form (you can find this in the "action" attribute of the HTML's form tag
+			string formParams = "uname=&pword=";
+			string cookieHeader;
+			WebRequest req = WebRequest.Create(formUrl);
+			req.ContentType = "application/x-www-form-urlencoded";
+			req.Method = "post";
+			byte[] bytes = Encoding.ASCII.GetBytes(formParams);
+			req.ContentLength = bytes.Length;
+			using (Stream os = req.GetRequestStream())
+			{
+				os.Write(bytes, 0, bytes.Length);
+			}
+			WebResponse resp = req.GetResponse();
+			cookieHeader = resp.Headers["Set-cookie"];
 
-            string pageSource;
-            string getUrl = "the url of the page behind the login";
-            WebRequest getRequest = WebRequest.Create(getUrl);
-            getRequest.Headers.Add("Cookie", cookieHeader);
-            WebResponse getResponse = getRequest.GetResponse();
-            using (StreamReader sr = new StreamReader(getResponse.GetResponseStream()))
-            {
-                pageSource = sr.ReadToEnd();
-            }
-            label1.Text = pageSource;
+			string pageSource;
+			string getUrl = "https://grades.nsd.org/showreportcard.php";
+			WebRequest getRequest = WebRequest.Create(getUrl);
+			getRequest.Headers.Add("Cookie", cookieHeader);
+			WebResponse getResponse = getRequest.GetResponse();
+			using (StreamReader sr = new StreamReader(getResponse.GetResponseStream()))
+			{
+				pageSource = sr.ReadToEnd();
+			}
+			label1.Text = pageSource;
         }
     }
 }
