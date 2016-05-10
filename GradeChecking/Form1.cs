4a7970072097c5efbac8f71ee6f9fb8a143f8bc2
Eric Zhu <ejz1999@yahoo.com>
2016-05-10 00:32:18 -0700

    Started project

---
+++ GradeChecking/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace GradeChecking
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string formUrl = "https://grades.nsd.org/index.php"; // NOTE: This is the URL the form POSTs to, not the URL of the form (you can find this in the "action" attribute of the HTML's form tag
+            string formParams = string.Format("email_address={0}&password={1}", "your email", "your password");
+            string cookieHeader;
+            WebRequest req = WebRequest.Create(formUrl);
+            req.ContentType = "application/x-www-form-urlencoded";
+            req.Method = "POST";
+            byte[] bytes = Encoding.ASCII.GetBytes(formParams);
+            req.ContentLength = bytes.Length;
+            using (Stream os = req.GetRequestStream())
+            {
+                os.Write(bytes, 0, bytes.Length);
+            }
+            WebResponse resp = req.GetResponse();
+            cookieHeader = resp.Headers["Set-cookie"];
+
+            string pageSource;
+            string getUrl = "the url of the page behind the login";
+            WebRequest getRequest = WebRequest.Create(getUrl);
+            getRequest.Headers.Add("Cookie", cookieHeader);
+            WebResponse getResponse = getRequest.GetResponse();
+            using (StreamReader sr = new StreamReader(getResponse.GetResponseStream()))
+            {
+                pageSource = sr.ReadToEnd();
+            }
+            label1.Text = pageSource;
+        }
+    }
+}
