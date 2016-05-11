1fadb1060c8730005ec3218361113c1a10b4cbc6
Eric Zhu <ejz1999@yahoo.com>
2016-05-11 00:37:53 -0700

    Can access the website and login now

---
+++ GradeChecking/GradeChecker.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace GradeChecking
+{
+    partial class GradeChecker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.buttonCheck = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(121, 37);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(100, 20);
+            this.textBoxUsername.TabIndex = 0;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(121, 86);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPassword.TabIndex = 1;
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Location = new System.Drawing.Point(35, 44);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(61, 13);
+            this.labelUsername.TabIndex = 2;
+            this.labelUsername.Text = "Student ID:";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(38, 92);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(56, 13);
+            this.labelPassword.TabIndex = 3;
+            this.labelPassword.Text = "Password:";
+            // 
+            // buttonCheck
+            // 
+            this.buttonCheck.Location = new System.Drawing.Point(95, 188);
+            this.buttonCheck.Name = "buttonCheck";
+            this.buttonCheck.Size = new System.Drawing.Size(75, 23);
+            this.buttonCheck.TabIndex = 4;
+            this.buttonCheck.Text = "Check";
+            this.buttonCheck.UseVisualStyleBackColor = true;
+            this.buttonCheck.Click += new System.EventHandler(this.buttonCheck_Click);
+            // 
+            // GradeChecker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.buttonCheck);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelUsername);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUsername);
+            this.Name = "GradeChecker";
+            this.Text = "Grade Checker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Button buttonCheck;
+
+    }
+}
+
---
+++ GradeChecking/GradeChecker.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Collections.Specialized;
+
+namespace GradeChecking
+{
+    public partial class GradeChecker : Form
+    {
+        public GradeChecker()
+        {
+            InitializeComponent();
+        }
+
+        static string GetSourceForMyShowsPage(string uname, string pword)
+        {
+            using (var client = new WebClientEx())
+            {
+                var values = new NameValueCollection
+                {
+                    { "uname", uname },
+                    { "pword", pword },
+                };
+                client.UploadValues("https://grades.nsd.org//login.php", values);
+                return client.DownloadString("https://grades.nsd.org/showreportcard.php");
+            }
+        }
+
+
+        static string ToTwoDigits(string date)
+        {
+            if (date.Length == 1)
+                date = "0" + date;
+            if (date.Length > 2)
+                date = date.Substring(date.Length - 2);
+            return date;
+        }
+
+        private void buttonCheck_Click(object sender, EventArgs e)
+        {
+            string shows = GetSourceForMyShowsPage(textBoxUsername.Text,textBoxPassword.Text);
+            string date = ToTwoDigits(DateTime.Now.Month.ToString()) + "-" + ToTwoDigits(DateTime.Now.Day.ToString()) + "-" + ToTwoDigits(DateTime.Now.Year.ToString());
+            if (shows.Contains(date))
+                labelPassword.Text = "Grade has been updated";
+        }
+    }
+
+    public class WebClientEx : WebClient
+    {
+        public CookieContainer CookieContainer { get; private set; }
+
+        public WebClientEx()
+        {
+            CookieContainer = new CookieContainer();
+        }
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            var request = base.GetWebRequest(address);
+            if (request is HttpWebRequest)
+            {
+                (request as HttpWebRequest).CookieContainer = CookieContainer;
+            }
+            return request;
+        }
+    }
+
+}
